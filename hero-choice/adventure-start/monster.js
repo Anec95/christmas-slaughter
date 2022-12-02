2aad0271cb5b89b56eb0048d063d87927b9eb83e
Anec95 <miori.thomas95@gmail.com>
2022-12-03 00:46:07 +0100

    new monster

--- hero-choice/adventure-start/monster.js
+++ hero-choice/adventure-start/monster.js
@@ -2,13 +2,37 @@ export const dataMonster = [
     {
     'name': 'Flame',
     'life': 6,
-    'defense': 20,
+    'defense': 15,
     'attack': 8
     },
     {
     'name': 'wolf pack',
     'life': 10,
-    'defense': 7,
+    'defense': 22,
     'attack': 14
+    },
+    // {
+    // 'name': 'wolf pack',
+    // 'life': 10,
+    // 'defense': 22,
+    // 'attack': 14
+    // },
+    // {
+    // 'name': 'wolf pack',
+    // 'life': 10,
+    // 'defense': 22,
+    // 'attack': 14
+    // },
+    // {
+    // 'name': 'wolf pack',
+    // 'life': 10,
+    // 'defense': 22,
+    // 'attack': 14
+    // },
+    {
+    'name': 'Grynch',
+    'life': 15,
+    'defense': 30,
+    'attack': 25
     }
 ]
