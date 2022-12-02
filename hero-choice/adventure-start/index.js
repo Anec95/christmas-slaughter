e8aea4e44a571c01191a4c0bed3c8fe2138e2f0b
Anec95 <miori.thomas95@gmail.com>
2022-12-02 16:35:40 +0100

    delete hero2 folder

---
+++ hero-choice/adventure-start/index.js
@@ -0,0 +1,302 @@
+import { dataItems } from "./items.js";
+console.log(dataItems);
+import { dataWeapons } from "./items.js";
+console.log(dataWeapons);
+import { dataHeroNames } from "./items.js";
+console.log(dataHeroNames);
+import { dataText } from "./quest-text.js";
+console.log(dataText);
+
+let playerName = document.getElementsByClassName('hero-name')[0];
+let intro = document.getElementById("introduction");
+let start = document.getElementById("starting-button");
+let optionOne = document.getElementById("option-1");
+let optionTwo = document.getElementById("option-2");
+let optionThree = document.getElementById("option-3");
+let optionFour = document.getElementById("option-4");
+let optionFive = document.getElementById("option-5");
+let optionSix = document.getElementById("option-6");
+
+let containerOne = document.getElementById("container-1");
+let containerTwo = document.getElementById("container-2");
+let containerThree = document.getElementById("container-3");
+
+
+let cancelButton = document.getElementsByClassName("fa-times-circle-o");
+let sureDiv = document.getElementById("items-yes-no");
+let sureDivText = document.getElementById("remove-text");
+let defenseDisplay = document.getElementsByClassName("numb-stat")[0];
+let attackDisplay = document.getElementsByClassName("numb-stat")[1];
+let objectTextDivs = document.getElementsByClassName('object-name');
+let weaponTextDivs = document.getElementsByClassName('weapon-name');
+let yesButton = document.getElementById('remove-yes');
+let noButton = document.getElementById('remove-no');
+let nameHeroChoicePanel = document.getElementsByClassName('enter-your-name-panel')[0];
+let textBoxHeroName = document.getElementById('text-area-box');
+let heroNameConfirmBtn = document.getElementById('corfim-button');
+
+
+heroNameConfirmBtn.onclick = function confirmName() {
+    let nameHero = textBoxHeroName.value;
+    console.log(nameHero)
+    if (nameHero === '') {
+        let indexName = Math.floor(Math.random() * 16) + 1;
+        playerName.innerText = `Name: ${dataHeroNames[indexName]}`;
+    } else {
+        playerName.innerText = `Name: ${nameHero}`;
+    };
+    nameHeroChoicePanel.style.display = 'none';
+}
+
+// playerName.innerText = "Name: " + prompt("What's your name?");
+defenseDisplay.innerText = 10 + Math.floor(Math.random() * 10) + 1;
+attackDisplay.innerText = 10 + dataWeapons[1].attack + Math.floor(Math.random() * 10) + 1;
+objectTextDivs[0].innerText = "Eggnog";
+weaponTextDivs[0].innerText = dataWeapons[1].name + ' - attack: +' + dataWeapons[1].attack;
+
+// attackDisplay.textContent = Number(attackDisplay.textContent) + 30;
+
+function hiddenContainer(container) {
+    container.style.display = "none";
+}
+
+hiddenContainer(containerOne);
+hiddenContainer(containerTwo);
+hiddenContainer(containerThree);
+
+function unveildContainer(uContainer) {
+    uContainer.style.display = "block";
+}
+
+function addItems(itemArray) {
+    for (let i = 0; i < objectTextDivs.length; i++) {
+        if (objectTextDivs[i].textContent === "...") {
+            objectTextDivs[i].innerText = itemArray.name;
+            break;
+        } else if (i === objectTextDivs.length-1 && objectTextDivs[i].textContent != "...") {
+            alert("you have no more space");
+        }
+
+    }
+}
+
+const health = document.getElementsByClassName("health")[0];
+let indexHealth = 7;
+
+function healthDispenser() {
+    let fullHearth = new Array(indexHealth).fill('<i class="fa fa-heart" aria-hidden="true"></i>');
+    let emptyHearth = new Array(7-indexHealth).fill('<i class="fa fa-heart-o" aria-hidden="true"></i>');
+
+    health.innerHTML = fullHearth.concat(emptyHearth).join("");
+}
+
+healthDispenser();
+
+function choice(element, container, uContainer, y, itemArray = "") {
+    element.onclick = function () {
+        hiddenContainer(container);
+        unveildContainer(uContainer);
+        //per togliere vita
+        indexHealth-=y;
+        
+        if (indexHealth <= 0) {
+            alert("You are died")
+            return;
+        }
+        healthDispenser();
+        //per aggiungere item
+        if (itemArray != "") {
+            addItems(itemArray);   
+        }
+             
+    }; 
+}
+
+
+choice(start, intro, containerOne, 0);
+choice(optionOne, containerOne, containerTwo, 1, dataItems[0]);
+choice(optionFour, containerTwo, containerThree, 3, dataItems[1]);
+
+
+
+
+
+
+
+
+
+
+function eventRemover() {
+    yesButton.removeEventListener('click', useItemFirstDiv);
+    yesButton.removeEventListener('click', useItemSecondDiv);
+    yesButton.removeEventListener('click', useItemThirdDiv);
+    yesButton.removeEventListener('click', useItemFourthDiv);
+    yesButton.removeEventListener('click', useItemFifthDiv);
+    yesButton.removeEventListener('click', useItemSixthDiv);
+    yesButton.removeEventListener('click', useItemSeventhDiv);
+    yesButton.removeEventListener('click', useItemEighthDiv);
+}
+
+function closeConfirmBox() {
+    sureDiv.style.display = 'none';
+}
+
+objectTextDivs[0].onclick = function() {
+    if ((indexHealth <= 6 && this.textContent === "Revive")
+        || (indexHealth <= 6 && this.textContent === "Candy apple")
+        || (indexHealth <= 6 && this.textContent === "Eggnog")) {
+        eventRemover()
+        sureDiv.style.display = 'block';
+        sureDivText.innerText = `Are you sure you want to use ${this.textContent}?`;
+        yesButton.addEventListener('click', useItemFirstDiv);
+        noButton.addEventListener('click', closeConfirmBox);
+    }    
+}
+
+objectTextDivs[1].onclick = function() {
+    if ((indexHealth <= 6 && this.textContent === "Revive")
+        || (indexHealth <= 6 && this.textContent === "Candy apple")
+        || (indexHealth <= 6 && this.textContent === "Eggnog")) {
+        eventRemover()
+        sureDiv.style.display = 'block';
+        sureDivText.innerText = `Are you sure you want to use ${this.textContent}?`;
+        yesButton.addEventListener('click', useItemSecondDiv);
+        noButton.addEventListener('click', closeConfirmBox);
+    }
+}
+
+objectTextDivs[2].onclick = function() {
+    if ((indexHealth <= 6 && this.textContent === "Revive")
+        || (indexHealth <= 6 && this.textContent === "Candy apple")
+        || (indexHealth <= 6 && this.textContent === "Eggnog")) {
+        eventRemover()
+        sureDiv.style.display = 'block';
+        sureDivText.innerText = `Are you sure you want to use ${this.textContent}?`;
+        yesButton.addEventListener('click', useItemThirdDiv);
+        noButton.addEventListener('click', closeConfirmBox);
+    }
+}
+
+objectTextDivs[3].onclick = function() {
+    if ((indexHealth <= 6 && this.textContent === "Revive")
+        || (indexHealth <= 6 && this.textContent === "Candy apple")
+        || (indexHealth <= 6 && this.textContent === "Eggnog")) {
+        eventRemover()
+        sureDiv.style.display = 'block';
+        sureDivText.innerText = `Are you sure you want to use ${this.textContent}?`;
+        yesButton.addEventListener('click', useItemFourthDiv);
+        noButton.addEventListener('click', closeConfirmBox);
+    }
+}
+
+objectTextDivs[4].onclick = function() {
+    if ((indexHealth <= 6 && this.textContent === "Revive")
+        || (indexHealth <= 6 && this.textContent === "Candy apple")
+        || (indexHealth <= 6 && this.textContent === "Eggnog")) {
+        eventRemover()
+        sureDiv.style.display = 'block';
+        sureDivText.innerText = `Are you sure you want to use ${this.textContent}?`;
+        yesButton.addEventListener('click', useItemFifthDiv);
+        noButton.addEventListener('click', closeConfirmBox);
+    }
+}
+
+objectTextDivs[5].onclick = function() {
+    if ((indexHealth <= 6 && this.textContent === "Revive")
+        || (indexHealth <= 6 && this.textContent === "Candy apple")
+        || (indexHealth <= 6 && this.textContent === "Eggnog")) {
+        eventRemover()
+        sureDiv.style.display = 'block';
+        sureDivText.innerText = `Are you sure you want to use ${this.textContent}?`;
+        yesButton.addEventListener('click', useItemSixthDiv);
+        noButton.addEventListener('click', closeConfirmBox);
+    }
+}
+
+objectTextDivs[6].onclick = function() {
+    if ((indexHealth <= 6 && this.textContent === "Revive")
+        || (indexHealth <= 6 && this.textContent === "Candy apple")
+        || (indexHealth <= 6 && this.textContent === "Eggnog")) {
+        eventRemover()
+        sureDiv.style.display = 'block';
+        sureDivText.innerText = `Are you sure you want to use ${this.textContent}?`;
+        yesButton.addEventListener('click', useItemSeventhDiv);
+        noButton.addEventListener('click', closeConfirmBox);
+    }
+}
+
+objectTextDivs[7].onclick = function() {
+    if ((indexHealth <= 6 && this.textContent === "Revive")
+        || (indexHealth <= 6 && this.textContent === "Candy apple")
+        || (indexHealth <= 6 && this.textContent === "Eggnog")) {
+        eventRemover()
+        sureDiv.style.display = 'block';
+        sureDivText.innerText = `Are you sure you want to use ${this.textContent}?`;
+        yesButton.addEventListener('click', useItemEighthDiv);
+        noButton.addEventListener('click', closeConfirmBox);
+    }
+}
+
+function whichItem(number) {
+    if (indexHealth < 6 &&
+        objectTextDivs[number].textContent === 'Revive') {
+        indexHealth += 2;        
+    } else if (indexHealth === 6 &&
+        objectTextDivs[number].textContent === 'Revive') {
+        indexHealth += 1;
+    } else if (indexHealth <= 6 &&
+        objectTextDivs[number].textContent === 'Candy apple') {
+        indexHealth += 1;        
+    } else if (indexHealth < 5 &&
+        objectTextDivs[number].textContent === 'Eggnog') {
+        indexHealth += 3;
+    } else if (indexHealth === 5  &&
+        objectTextDivs[number].textContent === 'Eggnog') {
+        indexHealth += 2;
+    } else if (indexHealth === 6  &&
+        objectTextDivs[number].textContent === 'Eggnog') {
+        indexHealth += 1;
+    }
+    objectTextDivs[number].innerText = '...';
+    healthDispenser();
+}
+
+function useItemFirstDiv() {
+    whichItem(0)
+    closeConfirmBox()
+}
+
+function useItemSecondDiv() {
+    whichItem(1)
+    closeConfirmBox()
+}
+
+function useItemThirdDiv() {
+    whichItem(2)
+    closeConfirmBox()    
+}
+
+function useItemFourthDiv() {
+    whichItem(3)
+    closeConfirmBox()    
+}
+
+function useItemFifthDiv() {
+    whichItem(4)
+    closeConfirmBox()    
+}
+
+function useItemSixthDiv() {
+    whichItem(5)
+    closeConfirmBox()    
+}
+
+function useItemSeventhDiv() {
+    whichItem(6)
+    closeConfirmBox()    
+}
+
+function useItemEighthDiv() {
+    whichItem(7)
+    closeConfirmBox()    
+}
---
+++ hero-choice/adventure-start/quest-text.js
@@ -0,0 +1,56 @@
+export const dataText = [
+    { //0
+        "name": "introduction",
+        "text": "It has now been 7 days, 17 hours and 5 minutes since you left your home in the south of the continent for the remote mountainsin the far north. A mission had been entrusted to you, you were to reach the Cristallino lake called to protect the Crumba village in the grip of danger, but now it seems a mere memory; you are standing in the mountains lost and frozen as you pass through a blizzard. The snow lashes your face and you struggle to continue... you brace yourself, and with arms outstretched to protect your snow-scratched face you continue on.<br>A glow appears in the distance.<br>With the last forces in your body you drag yourself towards hope. What seemed like a faint flame as you go along grows stronger. Figures of houses begin to appear before you, some are still lit up. You head determinedly towards the first one you encounter.",
+        "option1": "Knock on the door"        
+    },
+    {//1
+        "name": "after knock",
+        "text": "A chair crawls on the floor, it seems you have arrived just at the right moment. Then, through a peephole, an intimidated voice asks you to introduce yourself",
+        "option1": "You are dying for a nice hot dish. You say you are an emissary headed to Crumba and beg for a bite.", //OPTION 1
+        "option2": "" 
+    },
+    {//2
+        "name": "speech",
+        "text": "Good, good. The village you are looking for is just that. The only thing left to do is to show the warrant to the old man, who has now opened the door for you.",
+        "option1": "Extract warrant",
+        "option2": ""
+    },
+    {//3   //OPTION 1
+        "name": "option 1",
+        "text": "He lets you into the room where his wife and two daughters are already seated at the table. You gladly agree to sit in front of the fireplace and are immediately served with a hearty dish of hot stew. During dinner you have an interesting conversation with Feodor, the host, who sits with you.",
+        "option1": "What's going on?", //OPTION 2
+        "option2": "What advice can you give me?", //OPTION 3
+        "option3": "Who is the target?" //OPTION 4
+    },
+    {//4   //OPTION 2
+        "name": "option 2",
+        "text": "answer 1",
+        "option1": "", //OPTION 5
+        "option2": "" //OPTION 6
+    },
+    {//5   //OPTION 3
+        "name": "option 3",
+        "text": "answer 2",
+        "option1": "", //OPTION 7
+        "option2": "" //OPTION 8
+    },
+    {//6   //OPTION 4
+        "name": "option 4",
+        "text": "",
+        "option1": "", //OPTION 9
+        "option2": "" //OPTION 10
+    },
+    {//7   //OPTION 5
+        "name": "option 5",
+        "text": "",
+        "option1": "", //OPTION 11
+        "option2": "" //OPTION 12
+    },
+    {//8   //OPTION 6
+        "name": "option 6",
+        "text": "",
+        "option1": "", //OPTION 13
+        "option2": "" 
+    },
+];
